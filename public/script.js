87cdfaa50bf9885d6e099bcefe7807be47936aac
emrea-works <gelisimodakli@gmail.com>
2023-06-23 20:07:01 -0300

    finished instruction, works

--- public/script.js
+++ public/script.js
@@ -1,20 +1,24 @@
 /* global document, window, axios, supabase, ethers */
-const AUTH_API_URL = 'http://localhost:3000/api/auth';
-const SUPABASE_URL = 'replace_me';
-const SUPABASE_PUBLIC_ANON_KEY = 'replace_me';
+const AUTH_API_URL = "http://localhost:3000/api/auth";
+const SUPABASE_URL = "https://fcwvictmfvokccqffihm.supabase.co";
+const SUPABASE_PUBLIC_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjd3ZpY3RtZnZva2NjcWZmaWhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODczNTQ2NzUsImV4cCI6MjAwMjkzMDY3NX0.b-Dr7NzmjjJlDLAIhd6Px-QUWl0iMkpn4P4BWgyUYUg";
 
-const elError = document.getElementById('error');
-const elUser = document.getElementById('user');
-const elBtnMetamask = document.getElementById('auth-metamask');
-const elBtnGetUser = document.getElementById('getUser');
-const elBtnGetUserAnon = document.getElementById('getUserAnon');
-const _supabaseAnon = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY);
+const elError = document.getElementById("error");
+const elUser = document.getElementById("user");
+const elBtnMetamask = document.getElementById("auth-metamask");
+const elBtnGetUser = document.getElementById("getUser");
+const elBtnGetUserAnon = document.getElementById("getUserAnon");
+const _supabaseAnon = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLIC_ANON_KEY
+);
 let _supabaseAuthenticated;
 
 const handleApiPost = async (endpoint, params) => {
   const result = await axios.post(`${AUTH_API_URL}/${endpoint}`, params, {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
@@ -22,42 +26,42 @@ const handleApiPost = async (endpoint, params) => {
 };
 
 const requestMessage = (account, chain) =>
-  handleApiPost('request-message', {
+  handleApiPost("request-message", {
     address: account,
     chain,
-    networkType: 'evm',
+    networkType: "evm",
   });
 
 const verifyMessage = (message, signature) =>
-  handleApiPost('sign-message', {
+  handleApiPost("sign-message", {
     message,
     signature,
-    networkType: 'evm',
+    networkType: "evm",
   });
 
 const getUser = async () => {
   if (!_supabaseAuthenticated) {
     // eslint-disable-next-line no-alert
-    window.alert('You need to authenticate with Metamask first.');
+    window.alert("You need to authenticate with Metamask first.");
     return;
   }
-  const { data } = await _supabaseAuthenticated.from('users').select('*');
+  const { data } = await _supabaseAuthenticated.from("users").select("*");
   renderUser(data);
   renderError();
 };
 
 const getUserAnon = async () => {
-  const { data } = await _supabaseAnon.from('users').select('*');
+  const { data } = await _supabaseAnon.from("users").select("*");
   renderUser(data);
   renderError();
 };
 
 const connectToMetamask = async () => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
   const [accounts, chainId] = await Promise.all([
-    provider.send('eth_requestAccounts', []),
-    provider.send('eth_chainId', []),
+    provider.send("eth_requestAccounts", []),
+    provider.send("eth_chainId", []),
   ]);
 
   const signer = provider.getSigner();
@@ -69,10 +73,10 @@ const handleAuth = async () => {
   const { signer, chain, account } = await connectToMetamask();
 
   if (!account) {
-    throw new Error('No account found');
+    throw new Error("No account found");
   }
   if (!chain) {
-    throw new Error('No chain found');
+    throw new Error("No chain found");
   }
 
   const { message } = await requestMessage(account, chain);
@@ -81,38 +85,42 @@ const handleAuth = async () => {
 
   const { user } = await verifyMessage(message, signature);
 
-  _supabaseAuthenticated = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY, {
-    global: {
-      headers: {
-        Authorization: `Bearer ${user.token}`,
+  _supabaseAuthenticated = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLIC_ANON_KEY,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
       },
-    },
-  });
+    }
+  );
 
   renderUser(user);
   renderError();
 };
 
 const renderUser = (user) => {
-  elUser.innerHTML = user ? JSON.stringify(user, null, 2) : '';
+  elUser.innerHTML = user ? JSON.stringify(user, null, 2) : "";
 };
 
 const renderError = (error) => {
-  elError.innerHTML = error ? JSON.stringify(error.message, null, 2) : '';
+  elError.innerHTML = error ? JSON.stringify(error.message, null, 2) : "";
 };
 
 function init() {
-  elBtnMetamask.addEventListener('click', async () => {
+  elBtnMetamask.addEventListener("click", async () => {
     handleAuth().catch((error) => renderError(error));
   });
-  elBtnGetUser.addEventListener('click', async () => {
+  elBtnGetUser.addEventListener("click", async () => {
     getUser().catch((error) => renderError(error));
   });
-  elBtnGetUserAnon.addEventListener('click', async () => {
+  elBtnGetUserAnon.addEventListener("click", async () => {
     getUserAnon().catch((error) => renderError(error));
   });
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   init();
 });
